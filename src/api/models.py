e121cbe80cc0d7b6227a28a923f3f2ebdc8c90e8
fares-agour <faresabouagour@gmail.com>
2024-03-12 11:36:24 +0200

    create api model

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,22 @@
+import random
+import string
+
 from django.db import models
 
 
 # Create your models here.
 class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=55, unique=True)
+    votes_to_skip = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+def generate_code():
+    length = 6
+    while True:
+        code = ''.join(random.choices(string.ascii_letters, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
+

5d8c333b145bdf8cec81bae1e829b9bf64db8a9e
fares-agour <faresabouagour@gmail.com>
2024-03-12 23:58:11 +0200

    generate api

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
-from . import views
 from django.urls import path
+
+from .views import RoomView
 
 app_name = 'api'  # {% url 'api:index'%} used for this
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.index, name='index')
+    path('', RoomView.as_view()),
+    path('home/', RoomView.as_view())
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import Room
+from .serializers import RoomSerializer
 
 
-def index(request):
-    return HttpResponse("Hello world")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
 # Create your views here.
